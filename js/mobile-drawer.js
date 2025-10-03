92c703da89103b342c65b582571dc4085907782b
robrgf23-star <robertorgf23@gmail.com>
2025-10-03 17:32:56 -0400

    Pruebas para cambios y correciones de diseño

--- js/mobile-drawer.js
+++ js/mobile-drawer.js
@@ -1,8 +1,22 @@
-const btnHamb = document.getElementById('btnHamb');
-const nav = document.getElementById('mainNav');
+document.addEventListener('DOMContentLoaded', () => {
+  const btnHamb = document.getElementById('btnHamb');
+  const mainNav = document.querySelector('.menu-principal .navlist');
+  const secNav = document.querySelector('.menu-secundario .navlist');
 
-btnHamb.addEventListener('click', () => {
-  const expanded = btnHamb.getAttribute('aria-expanded') === 'true';
-  btnHamb.setAttribute('aria-expanded', !expanded);
-  nav.classList.toggle('open');
+  btnHamb.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    secNav.classList.toggle('show');
+  });
+
+  document.querySelectorAll('.menu-principal .navlist > li > a, .menu-secundario .navlist > li > a').forEach(item => {
+    item.addEventListener('click', (e) => {
+      const submenu = item.nextElementSibling;
+      if (submenu && (submenu.classList.contains('submenu') || submenu.classList.contains('mega-menu'))) {
+        if(window.innerWidth <= 992){
+          e.preventDefault();
+          submenu.classList.toggle('show');
+        }
+      }
+    });
+  });
 });
